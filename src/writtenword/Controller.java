0aac6b7e5b1212615f578a6f17dca032b48e3fb6
Marius-Juston <Marius.juston@hotmail.fr>
2019-11-04 19:14:47 -0600

    created drawing canvas

--- src/writtenword/Controller.java
+++ src/writtenword/Controller.java
@@ -1,4 +1,48 @@
 package writtenword;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
+public class Controller implements Initializable {
+
+	public Canvas canvas;
+	public ColorPicker colorChooser;
+	public StackPane stackPane;
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		canvas.heightProperty().bind(stackPane.heightProperty());
+		canvas.widthProperty().bind(stackPane.widthProperty());
+
+		colorChooser.setValue(Color.BLACK);
+
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+
+		gc.setStroke(Color.BLACK);
+		gc.setLineWidth(1);
+
+		canvas.setOnMousePressed(mouseEvent ->
+			{
+				gc.beginPath();
+				gc.lineTo(mouseEvent.getSceneX(), mouseEvent.getSceneY());
+				gc.stroke();
+			}
+		);
+
+		canvas.setOnMouseDragged(mouseEvent -> {
+			{
+				gc.lineTo(mouseEvent.getX(), mouseEvent.getY());
+				gc.stroke();
+			}
+		});
+
+		colorChooser.setOnAction(actionEvent -> gc.setStroke(colorChooser.getValue()));
+
+	}
 }
--- src/writtenword/Main.java
+++ src/writtenword/Main.java
@@ -5,19 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("written_word.fxml"));
-        primaryStage.setTitle("Written Word");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("written_word.fxml"));
+		primaryStage.setTitle("Written Word");
+		primaryStage.setScene(new Scene(root));
+//		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.show();
+	}
 }

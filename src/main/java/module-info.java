db32d63e01056c8127012a6c3ebdd0366d5359b2
Marius-Juston <Marius.juston@hotmail.fr>
2019-12-09 10:57:12 -0600

    removed unecessary google libararies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,9 @@
 module writtenword.main {
   requires javafx.controls;
   requires javafx.fxml;
-  requires google.api.client;
-  requires google.oauth.client.jetty;
-  requires google.oauth.client.java6;
-  requires google.oauth.client;
   requires com.calendarfx.view;
   requires javafx.swing;
 
   opens writtenword;
-  opens writtenword.google;
   opens writtenword.widget;
 }

8b72ef8f0471947a29eb5c9ff60006af2f7982ff
Marius-Juston <Marius.juston@hotmail.fr>
2019-12-09 10:52:32 -0600

    removed google photos library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,6 @@ module writtenword.main {
   requires javafx.controls;
   requires javafx.fxml;
   requires google.api.client;
-  requires google.api.services.calendar.v3.rev305;
-  requires google.http.client;
-  requires google.http.client.jackson2;
   requires google.oauth.client.jetty;
   requires google.oauth.client.java6;
   requires google.oauth.client;
